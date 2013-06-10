3852f0db0cd04266daf73b4adaa861a3e4206353
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2013-06-09 21:15:20 -0600

    Started to create the Quick Interview section and also started to modify Angular code

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,9 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('FourthInterview', ['FourthInterview.filters', 'FourthInterview.services', 'FourthInterview.directives', 'FourthInterview.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+var fourthInterviewApp = angular.module('FourthInterviewApp', []);
