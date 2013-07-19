0aaa19b130486169ee8dafe440935eb6f00b0ceb
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2013-07-19 00:17:04 -0500

    Changed a few things on presentation tier

--- src/main/webapp/app/js/controllers.js
+++ src/main/webapp/app/js/controllers.js
@@ -4,6 +4,14 @@ fourthInterviewApp.controller('appCtrl', ['$scope', function($scope) {
     $scope.appTitle = '4th Interview';
 }]);
 
+fourthInterviewApp.controller('sideMenuCtrl', ['$scope', function($scope) {
+    $scope.activeSideMenuItem = 'Quick Interview';
+
+    $scope.isSideMenuItemActive = function(sideMenuItem) {
+        return $scope.activeSideMenuItem == sideMenuItem;
+    };
+}]);
+
 fourthInterviewApp.controller('skillCtrl', ['$scope', 'skillService', function($scope, skillService) {
     $scope.skills = skillService.listAllSkills();
 }]);
--- src/main/webapp/app/js/services.js
+++ src/main/webapp/app/js/services.js
@@ -2,10 +2,16 @@
 
 fourthInterviewApp.factory('skillService', ['$http', function($http) {
     return {
-        listAllSkills: function(successCallback, failCallback) {
-            $http({method: 'GET', url: '/skills/listAll'}).
-                success(successCallback).
-                error(failCallback);
+        listAllSkills: function() {
+            // TODO: replace with AJAX call
+            return [
+                {name: 'Java', description: 'General knowledge about the Java Programming Language'},
+                {name: 'Design Patterns', description: 'Such as Factory, Singleton, Decorator, etc.'},
+                {name: 'HTML 5', description: 'All new features of HTML5 not present in the previous version'},
+                {name: 'Spring MVC', description: 'How to build web applications using Spring MVC'},
+                {name: 'CSS 3', description: 'Most commonly used selectors and styles'},
+                {name: 'PHP', description: 'General knowledge about the PHP Programming Language'},
+            ];
         }
     };
 }]);
