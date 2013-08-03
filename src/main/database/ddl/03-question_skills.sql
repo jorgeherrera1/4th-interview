4da05308572df8c5cedbe265eed74de8c4235c75
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2013-08-02 23:19:00 -0500

    Fixed primary key of skills table, again...

--- src/main/database/ddl/03-question_skills.sql
+++ src/main/database/ddl/03-question_skills.sql
@@ -1,9 +1,9 @@
 CREATE TABLE question_skills (
-   question_id int         NOT NULL,
-   skill_name    varchar(64) NOT NULL,
+   question_id int NOT NULL,
+   skill_id    int NOT NULL,
    
-   PRIMARY KEY (question_id, skill_name),
+   PRIMARY KEY (question_id, skill_id),
    
    CONSTRAINT question_skills_fk01 FOREIGN KEY (question_id) REFERENCES questions (question_id),
-   CONSTRAINT question_skills_fk02 FOREIGN KEY (skill_name)    REFERENCES skills (name)
+   CONSTRAINT question_skills_fk02 FOREIGN KEY (skill_id)    REFERENCES skills (skill_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/database/sql/data.sql
+++ src/main/database/sql/data.sql
@@ -3,21 +3,3 @@ insert into skills (name, description) values ('Java', 'Java programming languag
 insert into skills (name, description) values ('PHP', 'PHP programming language');
 insert into skills (name, description) values ('SQL', 'Structured query language');
 insert into skills (name, description) values ('PL/SQL', 'Oracle PL/SQL');
-
--- Questions
-insert into questions (question, expected_answer, question_level) values ('What is the difference between TRUNCATE and DELETE?', 'The DELETE may include a WHERE clause to limit the rows deleted, also the deletion can be rolled back whereas the TRUNCATE will delete all rows with no possibility of rolling back; additionally TRUNCATE will not fire any triggers', 'INTERMEDIATE');
-insert into questions (question, expected_answer, question_level) values ('What is the purpose of the VOLATILE keyword?', 'Every thread accessing a VOLATILE field will read its current value instead of (potentially) using a cached value', 'SENIOR');
-insert into questions (question, expected_answer, question_level) values ('How do you define a constant in PHP?', 'By using the DEFINE keyword, e.g., define("CONSTANT", "Hello World!");', 'JUNIOR');
-insert into questions (question, expected_answer, question_level) values ('What is the purpose of the PRAGMA AUTONOMOUS_TRANSTACTION clause?', 'A subprogram marked with this pragma can perform DML and commit/rollback without committing/rolling back the data of the calling transaction', 'SENIOR');
-
--- Question / skills
-insert into question_skills (question_id, skill_name) values (1, 'SQL');
-insert into question_skills (question_id, skill_name) values (2, 'Java');
-insert into question_skills (question_id, skill_name) values (3, 'PHP');
-insert into question_skills (question_id, skill_name) values (4, 'PL/SQL');
-
--- Candidates
-insert into candidates (name, age) values ('Homer Simpson',33);
-insert into candidates (name, age) values ('Superman', 28);
-insert into candidates (name, age) values ('Lionel Messi', 25);
-insert into candidates (name, age) values ('Sheldon Cooper', 30);
