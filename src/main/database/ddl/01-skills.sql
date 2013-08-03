e40065c905f3a0b53c21fe73a18efe3d62663af0
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2013-08-02 21:35:14 -0500

    Fixed primary key of skills table

--- src/main/database/ddl/01-skills.sql
+++ src/main/database/ddl/01-skills.sql
@@ -2,5 +2,5 @@ CREATE TABLE skills (
    skill_id    int         NOT NULL AUTO_INCREMENT,
    name        varchar(64) NOT NULL,
    description varchar(512),
-   PRIMARY KEY (name)
+   PRIMARY KEY (skill_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
